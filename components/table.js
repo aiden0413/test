c0db96a790f806c0bd501b6d11e086cb2b9785b3
aiden0413 <aiden0413@naver.com>
2025-05-22 08:52:02 +0900

    'table수정'

--- components/table.js
+++ components/table.js
@@ -1,5 +1,5 @@
 export default function Table(){
-    const css="border-t-2 border-b-2 border-r-2 border-black p-1";
+    const css="border-t-[clamp(0.1px,0.5vw,2px)] border-b-[clamp(0.1px,0.5vw,2px)] border-r-[clamp(0.1px,0.5vw,2px)] border-black p-[clamp(0.1px,0.5vw,2px)]";
 
     const heads=['','木1','木2','木3','木4'];
 
@@ -18,17 +18,17 @@ export default function Table(){
     let index=0;
 
     return (
-        <div className="p-4 bg-white">
-        <div className="relative p-4 border-3 border-black">
+        <div className="w-full max-w-md p-[clamp(1px,4vw,4px)] bg-white">
+        <div className="relative p-[clamp(1px,4vw,4px)] border-[clamp(1px,4vw,4px)] border-black">
             <div className="text-center">
                 <div className="my-3 text-[clamp(1px,3vw,15px)] text-black">김로켓님의 사주<br/></div>
                 <div className="my-3 text-[clamp(1px,4vw,20px)] text-black">1980년 8월27일 08:10</div>
             </div>
-            <table className="text-center table-auto text-black w-full">
+            <table className="text-center table-fixed text-black w-full h-full border-collapse">
                 <thead>
                     <tr>
                         {heads.map((head,i)=>(
-                            <th key={i} className="border-r-2 border-black">{head}</th>
+                            <th key={i} className="border-r-[clamp(0.1px,0.5vw,2px)] border-black text-[clamp(1px,6vw,20px)]">{head}</th>
                         ))}
                     </tr>
                 </thead>
@@ -39,20 +39,21 @@ export default function Table(){
                                 if(value.length==3){
                                     const bgColor=color1[index];
                                     const textColor=color2[index];
-                                    const borderColor= color1[index]==='bg-white' ? 'border border-black' : '';
+                                    const borderColor= color1[index]==='bg-white' ? 'border-[clamp(0.1px,0.5vw,1px)] border-black' : '';
                                     index++;
-                                    console.log(bgColor,textColor,borderColor)
                                     return (
                                         <td key={`${idx1}-${idx2}`} className={css}>
-                                            <div className={`${bgColor} rounded-2xl p-2 ${textColor} ${borderColor}`}>
+                                            <div className={`aspect-square flex items-center justify-center ${bgColor} rounded-[clamp(0.1px,4vw,20px)] p-[clamp(0.1px,2vw,4px)] ${textColor} ${borderColor} `}>
+                                                <div>
                                                 {value.map((text,idx3)=>{
                                                     if(idx3===1){
-                                                        return <div key={`${idx1}-${idx2}-${idx3}`} className="text-[clamp(1px,6vw,20px)]">{text}</div>
+                                                        return <div key={`${idx1}-${idx2}-${idx3}`} className="text-[clamp(1px,5vw,20px)]">{text}</div>
                                                     }
                                                     else{
-                                                        return <div key={`${idx1}-${idx2}-${idx3}`} className="text-[clamp(1px,3vw,8px)]">{text}</div>
+                                                        return <div key={`${idx1}-${idx2}-${idx3}`} className="text-[clamp(1px,2vw,8px)]">{text}</div>
                                                     }
                                                 })}
+                                                </div>
                                             </div>
                                         </td>
                                     )
--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ export default function Home() {
         src="/운세박사 코딩테스트/Group 1410141690-1.png" 
         className="w-full h-auto object-cover"
       />
-      <div className="absolute top-[31%] left-[15%] text-center text-black text-[clamp(1px,5vw,20px)]">
+      <div className="absolute top-[31%] left-[15%] text-center text-black text-[clamp(1px,5vw,22px)]">
         이제 본격적으로<br/>
         OO님의 사주팔자를<br/>
         분석해볼 차례네요.
       </div>
-      <div className="absolute top-[49%] left-[12%] text-center text-black text-[clamp(1px,5vw,20px)]">
+      <div className="absolute top-[49%] left-[12%] text-center text-black text-[clamp(1px,5vw,22px)]">
         제가 oo님의 사주를<br/>
         보기 쉽게 표로 정리했어요
       </div>
